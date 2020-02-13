c47be82203fc8662f50bd8bf92157bd9f1cd4703
Antonio Oropeza <antonio@empleosti.com.mx>
2020-02-13 13:56:46 -0600

    undo namespace on routes for controllers

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'posts', 'namespace' => 'JaopMX\FaqPackage\Controllers'], function(){
+Route::group(['prefix' => 'posts'], function(){
 	Route::get('/', 'PostController@index')->name('dashboard');
     Route::get('/create', 'PostController@create')->name('posts.create');
     Route::post('/create', 'PostController@store')->name('posts.store');
@@ -11,13 +11,13 @@ Route::group(['prefix' => 'posts', 'namespace' => 'JaopMX\FaqPackage\Controllers
     Route::get('/toggle', 'PostController@toggle')->name('posts.toggle');
 });
 
-Route::group(['prefix' => 'categories', 'namespace' => 'JaopMX\FaqPackage\Controllers'], function(){
+Route::group(['prefix' => 'categories'], function(){
     Route::get('/', 'CategoryController@index')->name('categories.index');
     Route::post('/store', 'CategoryController@store')->name('categories.store');
     Route::post('/update', 'CategoryController@update')->name('categories.update');
 });
 
-Route::group(['prefix' => 'resources', 'namespace' => 'JaopMX\FaqPackage\Controllers'], function(){
+Route::group(['prefix' => 'resources'], function(){
     Route::get('/', 'ResourceController@show')->name('resources.show');
     Route::post('/upload', 'ResourceController@store')->name('resources.store');
 });
