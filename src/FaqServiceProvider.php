5936416d7112a202b7386a8a26d46236736e6128
Antonio Oropeza <antonio@empleosti.com.mx>
2020-02-25 18:17:10 -0600

    add config and can publish config file

--- src/FaqServiceProvider.php
+++ src/FaqServiceProvider.php
@@ -11,10 +11,21 @@ class FaqServiceProvider extends ServiceProvider
 	{
 		$this->registerRoutes();
 		$this->registerResources();
+		
+		$this->publishes([
+			__DIR__.'/../config/faq.php' => config_path('faq.php')
+		], 'faq_config');
 
 		$this->publishes([
-			__DIR__ . '/migrations/' => database_path('migrations')
-		], 'migrations');
+			__DIR__.'/migrations/' => database_path('migrations')
+		], 'faq_migrations');
+	}
+
+	public function register()
+	{
+		$this->mergeConfigFrom(
+            __DIR__.'/../config/faq.php', 'faq'
+		);
 	}
 
 	public function registerRoutes()
